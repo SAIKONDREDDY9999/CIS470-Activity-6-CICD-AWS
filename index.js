7ed18365f4e539d39904e94983e696d259abf863
SAIKONDREDDY9999 <saisurendra.kondreddy@gmail.com>
2025-04-03 16:36:27 -0400

    Update index.js
    Simplified the Lambda function to just return a "Hello from Lambda" message for testing CI/CD pipeline deployment.

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const express = require('express');
-const serverless = require('serverless-http');
-
-const app = express();
-
-const { classifyTriangle } = require('./classifyTriangle');
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static('public'));
-
-app.get('/default/CIS470-Activity-6', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-})
-
-app.post('/default/CIS470-Activity-6/check-triangle', (req, res) => {
-    const { side1, side2, side3 } = req.body;
-    const result = classifyTriangle(side1,side2,side3);
-    res.json({ result });
-})
-
-module.exports.handler = serverless(app);
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda! Successfully deployed via GitHub Actions!"),
+  };
+};
